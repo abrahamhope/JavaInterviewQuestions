f1f06e44cb4bf47f8e789cc1b8c41e66ded631c2
abrahamhope <abrahamisaachope@gmail.com>
2020-03-30 15:28:43 -0500

    moveZeros

--- src/J15_MoveZerosToEnd.java
+++ src/J15_MoveZerosToEnd.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 public class J15_MoveZerosToEnd {
 
-    public static int[] moveZeros( int[]  arr ) {
-
-        ArrayList<Integer> list = new ArrayList<>();
-        int countZero = 0;
-        for(int each: arr) {
-            list.add(each);
-            countZero+= (each==0)?1:0;
+    public static void main(String[] args) {
+        moveZeros(new int[]{1,3,0,7,9,0,7,0});
+    }
+    public static void moveZeros(int[] arr){
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] !=0){
+                arr[index] = arr [i];
+                index++;
+            }
         }
 
-        list.removeAll(Arrays.asList(0));
-        arr = new int[arr.length];
-        for(int i=0; i <arr.length-countZero; i++ ) {
-            arr[i] = list.get(i);
+        for (int i = index; i < arr.length; i++) {
+            arr [i]=0;
         }
-        return arr;
     }
 }
