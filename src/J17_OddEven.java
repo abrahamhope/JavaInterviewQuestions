e38205d28fe21fcb7e25151d0e586c3d20c6de10
abrahamhope <abrahamisaachope@gmail.com>
2020-03-30 15:41:49 -0500

    interview

---
+++ src/J17_OddEven.java
@@ -0,0 +1,5 @@
+public class J17_OddEven {
+    public  static  String  identifyOddEven( int  n ) {
+        return  n%2==0 ? "Even" : "odd" ;
+    }
+}
---
+++ src/J18_DivideWithoutOperator.java
@@ -0,0 +1,18 @@
+public class J18_DivideWithoutOperator {
+    public static void devides(int num1, int num2) {
+
+        if(num2==0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+
+        System.out.print(num1 +" devid by "+num2 +" is: ");
+        int count =0;
+        while(num1 >= num2) {
+            num1 -= num2;
+            count++;
+        }
+
+        System.out.println(count+" and remainder is "+num1);
+    }
+}
